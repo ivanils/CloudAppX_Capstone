ff0d88d840cbc6b548be8d9df557359ba3f7b5f4
Ivan Llanos <ivan.llanos.santamaria@gmail.com>
2026-04-15 16:25:45 +1000

    solved

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,19 +1,16 @@
 import axios from 'axios';
 
 // base url of the fastapi backend )
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = 'https://cloudappx-capstone.onrender.com';
 export default API_URL;
 
 // Fetches the prioritized tickets from the API.
 export const fetchTickets = async () => {
-    const cleanURL = API_URL ? API_URL.replace(/\/$/, "") : "";
-    const endpoint = `${cleanURL}/api/tickets`;
     try {
         // Call the endpoint defined in main.py
-        const response = await axios.get(endpoint);
+        const response = await axios.get('${API_URL}/api/tickets');
         return response.data;
     } catch (error) {
-        alert(`Intento en: ${endpoint} \nFallo: ${error.message}`);
         console.error("Error connecting to the API:", error);
         throw error;
     }
